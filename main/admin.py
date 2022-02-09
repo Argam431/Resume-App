243a632ce6bd6bda2fc44ff9d2c136c5f14ca9d3
Argam431 <argam.manukyan.1994@mail.ru>
2022-02-10 00:35:29 +0400

    resume app

---
+++ main/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from . models import (
+    UserProfile,
+    ContactProfile,
+    Certificate,
+    Skill
+    )
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+	list_display = ('id', 'user')
+
+@admin.register(ContactProfile)
+class ContactAdmin(admin.ModelAdmin):
+	list_display = ('id', 'timestamp', 'name',)
+
+
+@admin.register(Certificate)
+class CertificateAdmin(admin.ModelAdmin):
+    list_display = ('id','name')
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('id','name','score')
